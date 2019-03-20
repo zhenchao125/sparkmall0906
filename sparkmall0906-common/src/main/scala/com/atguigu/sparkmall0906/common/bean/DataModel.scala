db2746e404f51a4281fdcfe977f431c37d22d00a
John Doe <30752740@qq.com>
2019-03-20 11:12:03 +0800

    Initial commit

---
+++ sparkmall0906-common/src/main/scala/com/atguigu/sparkmall0906/common/bean/DataModel.scala
@@ -0,0 +1,70 @@
+package com.atguigu.sparkmall0906.common.bean
+
+/**
+  * 用户表
+  *
+  * @param user_id      用户 id
+  * @param username     用户的登录名
+  * @param name         用户的昵称或真实名
+  * @param age          用户年龄
+  * @param professional 用户的职业
+  * @param gender       用户的性别
+  */
+case class UserInfo(user_id: Long,
+                    username: String,
+                    name: String,
+                    age: Int,
+                    professional: String,
+                    gender: String)
+
+/**
+  * 城市表
+  *
+  * @param city_id   城市 id
+  * @param city_name 城市名
+  * @param area      城市区域
+  */
+case class CityInfo(city_id: Long,
+                    city_name: String,
+                    area: String)
+
+/**
+  * 产品表
+  *
+  * @param product_id   商品的ID
+  * @param product_name 商品的名称
+  * @param extend_info  商品额外的信息
+  */
+case class ProductInfo(product_id: Long,
+                       product_name: String,
+                       extend_info: String)
+
+/**
+  * 用户访问动作表
+  *
+  * @param date               用户点击行为的日期
+  * @param user_id            用户的ID
+  * @param session_id         Session的ID
+  * @param page_id            某个页面的ID
+  * @param action_time        动作的时间点
+  * @param search_keyword     用户搜索的关键词
+  * @param click_category_id  某一个商品品类的ID
+  * @param click_product_id   某一个商品的ID
+  * @param order_category_ids 一次订单中所有品类的ID集合
+  * @param order_product_ids  一次订单中所有商品的ID集合
+  * @param pay_category_ids   一次支付中所有品类的ID集合
+  * @param pay_product_ids    一次支付中所有商品的ID集合
+  */
+case class UserVisitAction(date: String,
+                           user_id: Long,
+                           session_id: String,
+                           page_id: Long,
+                           action_time: String,
+                           search_keyword: String,
+                           click_category_id: Long,
+                           click_product_id: Long,
+                           order_category_ids: String,
+                           order_product_ids: String,
+                           pay_category_ids: String,
+                           pay_product_ids: String,
+                           city_id: Long)
---
+++ sparkmall0906-common/src/main/scala/com/atguigu/sparkmall0906/common/util/ConfigurationUtil.scala
@@ -0,0 +1,29 @@
+package com.atguigu.sparkmall0906.common.util
+
+import org.apache.commons.configuration2.{FileBasedConfiguration, PropertiesConfiguration}
+import org.apache.commons.configuration2.builder.FileBasedConfigurationBuilder
+import org.apache.commons.configuration2.builder.fluent.Parameters
+
+import scala.collection.mutable
+
+object ConfigurationUtil {
+    // 这个 map 用来存储配置文件名和在这个文件内定义的配置
+    private val configs = mutable.Map[String, FileBasedConfiguration]()
+    
+    // 参数是配置文件名
+    def apply(propertiesFileName: String) = {
+        // 根据配置文件名来获取来获取对应的配置.
+        // 如果 map 中存在这一的配置文件, 则读取配置文件的内容并更新到 map 中
+        configs.getOrElseUpdate(
+            propertiesFileName,
+            new FileBasedConfigurationBuilder[FileBasedConfiguration](classOf[PropertiesConfiguration]).configure(new Parameters().properties().setFileName(propertiesFileName)).getConfiguration)
+    }
+    
+    def main(args: Array[String]): Unit = {
+        // 测试是否可用
+        val conf = ConfigurationUtil("config.properties")
+        println(conf.getString("jdbc.user"))
+        println(conf.getInt("redis.port"))
+        
+    }
+}
